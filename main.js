c73f60d10045b4e4e964eaa021be404e8ecdc97b
fanqiexiaohuoguo <huang0616ke@163.com>
2019-08-31 10:47:37 +0800

    代码重构

--- main.js
+++ main.js
@@ -2,31 +2,36 @@ var allButtons=$('#buttons>button')
 for(let i=0;i<allButtons.length;i++){
   $(allButtons[i]).on('click',function(event){
     var index=$(event.currentTarget).index()
-    var p=index*(-300)
+    var p=index*(-600)
     $('#images').css({
       transform:'translate('+p+'px)'
     })
     n=index
-    allButtons.eq(n).addClass('red').siblings('.red').removeClass('red')
+    activeButton(allButtons.eq(n))
   })
 }
+
 var n=0
 var size=allButtons.length
-allButtons.eq(n%size).trigger('click')
-.addClass('red').siblings('.red').removeClass('red')
-var timeId=setInterval(()=>{
-  n+=1
-  allButtons.eq(n%size).trigger('click')
-.addClass('red').siblings('.red').removeClass('red')
-},1000)
+playSlide(n%size)
+activeButton(allButtons.eq(n%size))
+var timeId=setTimer()
 $('.window').on('mouseenter',function(){
   window.clearInterval(timeId)
 })
 $('.window').on('mouseleave',function(){
-  timeId=setInterval(()=>{
-    n+=1
-    allButtons.eq(n%size).trigger('click')
-  .addClass('red').siblings('.red').removeClass('red')
-  },1000)
+  timeId=setTimer()
 })
 
+function activeButton($button){
+  $button.addClass('red').siblings('.red').removeClass('red')
+}
+function playSlide(index){
+  allButtons.eq(index).trigger('click')
+}
+function setTimer(){
+  return setInterval(()=>{
+    n+=1
+    playSlide(n%size)
+  },3000)
+}
